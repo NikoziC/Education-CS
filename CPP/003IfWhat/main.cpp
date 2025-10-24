1f1e34d5902bd9ca383c76a70c2f989d6c41a4e8
Nikita Kozhin <nik.kozhin.iva@gmail.com>
2025-10-24 14:36:01 +0300

    003Ifwhat commit 2

--- CPP/003IfWhat/main.cpp
+++ CPP/003IfWhat/main.cpp
@@ -1,11 +1,32 @@
-#include<iostream>
+#include <iostream> // Подключаем стандартную библиотеку CPP Ввод-вывод. 
 
-int main() //Обязательная функция в CPP, с которой начинается выполнение программы.
+
+int main() // Обязательная функция в CPP, с которой начинается выполнение программы.
 {
-	char answer;
-	std::cout << "Welcome to the Richard's cave!\n";
+	std::string answer; // Задаем переменную, которая будет хранить ответ пользователя.
+	
+	std::cout << "Welcome to the Richard's cave!\n"; // Выводим приветственные сообщения в консоль через функцию cout из std.
 	std::cout << "Do you have a gift for our Lord?\n";
-	std::cout << "y/n? ";
-	std::cin >> answer;
+	
+	while (true)
+	{
+		std::cout << "y/n? ";
+		std::cin >> answer; // Ожидаем ответа от пользователя и принимаем его значения в переменную answer, при помощи функции cin из std.
+		
+		if (answer == "y" || answer == "Y") {
+			std::cout << "Hoho, My Darling, you're pretty smart.\n";
+			break;
+		}
+		else if (answer == "n" || answer == "N") {
+			std::cout << "~YOU ARE DEAD.~\n";
+			break;
+		}
+		else {
+			std::cout << "WHAT DO YOU MEAN???\n";
+			std::cin.clear();
+		}
+
+	}
+
 	return 0;
 }
